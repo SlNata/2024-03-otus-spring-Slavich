1711cdc91be50e96640a1529389b05a9807c64c2
SlNata <hatallia_0214@mail.ru>
2024-04-11 18:25:08 +0300

    Изменения во втором домашнем задании после проверки

--- hw02/src/main/java/ru/otus/hw/service/ResultServiceImpl.java
+++ hw02/src/main/java/ru/otus/hw/service/ResultServiceImpl.java
@@ -1,12 +1,12 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 import ru.otus.hw.config.TestConfig;
 import ru.otus.hw.domain.TestResult;
 
 @RequiredArgsConstructor
-@Service
+@Component
 public class ResultServiceImpl implements ResultService {
 
     private final TestConfig testConfig;
